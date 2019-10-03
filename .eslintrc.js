dce21bc49f4e3b6aaa73d7128f33a91cdf972fec
Logan McAnsh <logan@mcan.sh>
2019-10-02 20:52:09 -0400

    chore(deps): bump to latest
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,22 +6,8 @@ module.exports = {
       typescript: {},
     },
   },
-  overrides: [
-    {
-      files: ['*.d.ts'],
-      rules: {
-        'spaced-comment': ['error', 'always', { markers: ['/'] }],
-      },
-    },
-    {
-      files: ['*.js', '.*.js'],
-      rules: { '@typescript-eslint/no-var-requires': 'off' },
-    },
-  ],
   rules: {
     'arrow-body-style': ['error', 'as-needed'],
-    'promise/prefer-await-to-callbacks': 'off',
-    'promise/prefer-await-to-then': 'off',
     'import/no-extraneous-dependencies': [
       'error',
       {
